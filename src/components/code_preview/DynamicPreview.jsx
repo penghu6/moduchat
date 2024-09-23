c5684c105c1b9d9d244c03fc9d85cba68886164e
penghu <407479752@qq.com>
2024-09-23 16:32:55 +0800

    字体图标

--- src/components/code_preview/DynamicPreview.jsx
+++ src/components/code_preview/DynamicPreview.jsx
@@ -34,6 +34,7 @@ function DynamicPreview({ codeBlocks }) {
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
           <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
           <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
           <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
