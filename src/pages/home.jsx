c592f6c294e0014b7da00a56fd5c59d51a06ead5
penghu <407479752@qq.com>
2024-10-13 23:01:05 +0800

    debug

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,6 @@ import { Layout } from 'antd';
 import UserChatBox from "../components/UserChatBox.jsx";
 import Right from "../components/ComponentToolbox.jsx";
 import CodeWorkspace from "../components/CodeWorkspace.jsx";
-import Test from "../components/test.jsx";
 import "../css/App.css";
 
 const { Content: AntdContent } = Layout;
